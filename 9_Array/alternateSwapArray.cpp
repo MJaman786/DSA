6c42d36718a5303dda717020303bbe81c3f130a5
MJaman786 <112769441+MJaman786@users.noreply.github.com>
2024-09-11 11:21:28 +0530

    Update alternateSwapArray.cpp

--- 9_Array/alternateSwapArray.cpp
+++ 9_Array/alternateSwapArray.cpp
@@ -1,38 +1,46 @@
 #include<iostream>
 using namespace std;
-void alternateSwap(int arr[], int size){
-    int start = 0;
-    int end = start + 1;
 
-    for (int i = 0; i < size/2; i++)
-    {
+void alternateSwap(int arr[], int size){
+    int start = 0; 
+    int end = start + 1;
+    for(int i=0; i<size/2; i++){
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-        start += 2;
-        end += 2;
+        
+        start = start + 2;
+        end = end + 2;
     }
-    for (int i = 0; i < size; i++)
-    {
+    cout<<"After  : ";
+    for(int i=0; i<size; i++){
         cout<<arr[i]<<" ";
+        if(i % 2 == 1 && i != size - 1){
+            cout<<", ";
+        }else{
+            cout<<" ";
+        }
     }
-    
+    cout<<endl;
 }
+
 int main(){
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr[]={1,2,3,4,5,6,7,8};
     int size = sizeof(arr)/sizeof(arr[0]);
     cout<<"Before : ";
-    for (int i = 0; i < size; i++)
-    {
+    for(int i=0; i<size; i++){
         cout<<arr[i]<<" ";
+        if(i % 2 == 1 && i != size - 1){
+            cout<<", ";
+        }else{
+            cout<<" ";
+        }
     }
-    
     cout<<endl;
-    cout<<"After : ";
     alternateSwap(arr, size);
     return 0;
 }
 /*
-Before : 1 2 3 4 5 6 7 8 9 10
-After : 2 1 4 3 6 5 8 7 10 9
+Before : 1  2 , 3  4 , 5  6 , 7  8  
+After  : 2  1 , 4  3 , 6  5 , 8  7
 */ 
